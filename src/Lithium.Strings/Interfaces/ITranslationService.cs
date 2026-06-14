95a41a89b058555103bb4dab81c66d07d4a131f3
Chris Grassi <chris_grassi@proton.me>
2026-06-14 14:52:50 -0400

    ITranslationService doc comments

--- src/Lithium.Strings/Interfaces/ITranslationService.cs
+++ src/Lithium.Strings/Interfaces/ITranslationService.cs
@@ -5,11 +5,37 @@ using StringArgument = (string key, object value);
 
 namespace Lithium.Strings;
 
+/// <summary>
+/// Interface for a service used to translate strings.
+/// </summary>
 public interface ITranslationService {
-	public void Reload();
-	public string Translate(string key, params StringArgument[] args);
-	public IEnumerable<string> GetAllStringAddresses();
+	/// <summary>
+	/// Convert registered string resources into usable translatable units.
+	/// </summary>
+	void Reload();
+	/// <summary>
+	/// Translates the string by replacing parameters with the given values.
+	/// </summary>
+	/// <param name="key">The key of the string to translate.</param>
+	/// <param name="args">Tuples where the first item is the placeable name and the second is the value.</param>
+	/// <returns>Translated string with parameters replaced.</returns>
+	string Translate(string key, params StringArgument[] args);
+	/// <summary>
+	/// Fetches a list of all string addresses in the service.
+	/// </summary>
+	/// <returns>List of string addresses.</returns>
+	IEnumerable<string> GetAllStringAddresses();
 
-	public bool RegisterResource(string directory);
-	public bool RegisterResource(Assembly assembly);
+	/// <summary>
+	/// Registers a collection of external string resources within a directory.
+	/// </summary>
+	/// <param name="directory">Path of the directory containing the string resources.</param>
+	/// <returns>True if the resource was successfully registered.</returns>
+	bool RegisterResource(string directory);
+	/// <summary>
+	/// Registers string resources embedded within an assembly.
+	/// </summary>
+	/// <param name="assembly">Assembly containing the string resources.</param>
+	/// <returns>True if the resource was successfully registered.</returns>
+	bool RegisterResource(Assembly assembly);
 }
